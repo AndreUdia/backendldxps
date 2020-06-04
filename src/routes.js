d19f27adcd8bb8034fd749f826cedba1d597829b
Cabomaldade <soutom@yahoo.com.br>
2020-06-04 20:10:14 -0300

    Resolvido problema de CORS

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const VendedorController = require('./controllers/VendedorController');
 const ClienteController = require('./controllers/ClienteController');
+const cors = require('cors');
 
 const routes = express.Router();
 
 // crud vendedores
-routes.get('/vendedores', VendedorController.retornaVendedores);
-routes.post('/vendedores', VendedorController.gravaVendedor);
-routes.put('/vendedores/:cdvend', VendedorController.editarVendedor);
+routes.get('/vendedores', cors(), VendedorController.retornaVendedores);
+routes.post('/vendedores', cors(), VendedorController.gravaVendedor);
+routes.put('/vendedores/:cdvend', cors(), VendedorController.editarVendedor);
 routes.delete('/vendedores/:cdvend', VendedorController.excluirVendedor);
 
 // crud clientes
-routes.post('/vendedor/:cdvend/clientes', ClienteController.gravaCliente); // uuid do vendedor informado na rota
-routes.get('/vendedor/:cdvend/clientes', ClienteController.buscaClientesDoVendedor); // uuid do vendedor informado na rota
-routes.get('/clientes', ClienteController.retornaClientes);
-routes.delete('/clientes/:cdcl', ClienteController.excluirCliente);
-routes.put('/clientes/:cdcl', ClienteController.editarCliente);
+routes.post('/vendedor/:cdvend/clientes',cors(), ClienteController.gravaCliente); // uuid do vendedor informado na rota
+routes.get('/vendedor/:cdvend/clientes', cors(), ClienteController.buscaClientesDoVendedor); // uuid do vendedor informado na rota
+routes.get('/clientes', cors(), ClienteController.retornaClientes);
+routes.delete('/clientes/:cdcl', cors(), ClienteController.excluirCliente);
+routes.put('/clientes/:cdcl', cors(), ClienteController.editarCliente);
 
 module.exports = routes;
