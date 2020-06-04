78e7de4cefa2c31ba98d3b8501b2c42507289078
Cabomaldade <soutom@yahoo.com.br>
2020-06-04 19:20:05 -0300

    Banco postgres reconfigurado com docker

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
     dialect: 'postgres', // ruby.db.elephantsql.com - nuvem
     host: 'localhost', // 
     username: 'postgres', // 'arivaqxt' - nuvem
-    password: 'Postgres2018!', // '5D_VaKVptmJwojKem6Slo96BILm3jBKl' - nuvem
-    database: 'sqlnode',
+    password: '12345', // '5D_VaKVptmJwojKem6Slo96BILm3jBKl' - nuvem
+    database: 'postgres',
     //database: 'postgres://arivaqxt:5D_VaKVptmJwojKem6Slo96BILm3jBKl@ruby.db.elephantsql.com:5432/arivaqxt', // nuvem
     define: {
         timestamps: true, // creatAt, updated_at
