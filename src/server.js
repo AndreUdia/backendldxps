24212cbcf967190e461441ba40bbb99f0254cbce
Cabomaldade <soutom@yahoo.com.br>
2020-06-04 22:15:09 -0300

    Pequenos ajustes de CORS

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 require('./database');  // Com isso o banco fica ativo para os Models
 
 const app =  express();
+app.use(cors())
 
 app.use(express.json());
 app.use(routes);
